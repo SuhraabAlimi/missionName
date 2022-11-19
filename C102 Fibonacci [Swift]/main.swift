d1f49658cc8b2c02726e1dd5e81565ee99328bd8
SuhraabAlimi <70545301+SuhraabAlimi@users.noreply.github.com>
2022-11-18 21:44:40 -0600

    C102

---
+++ C102 Fibonacci [Swift]/main.swift
@@ -0,0 +1,29 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+
+func completeFibnachi(n:Int) -> Int {
+    print("Calculating fibonacci(\(n))...")
+      if (n <= 2)
+      {
+          print("Terminal case, returning 1.")
+          return 1
+      }
+      else {
+          let appendNext = completeFibnachi(n:n-1) + completeFibnachi(n:n-2)
+          print("Non-terminal case, returning \(appendNext).")
+          return appendNext
+      }
+}
+func main()
+{
+print("The final result is: \(completeFibnachi(n:n)).")
+}
+main()
