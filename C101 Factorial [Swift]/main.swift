63b21fe77e61c03388047aaf1dcb8c198df9bd85
SuhraabAlimi <70545301+SuhraabAlimi@users.noreply.github.com>
2022-11-18 21:21:49 -0600

    Facotrial

---
+++ C101 Factorial [Swift]/main.swift
@@ -0,0 +1,34 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+
+
+func calculateFactorial(from n: Int) -> Int
+{
+    print("Calculating factorial(\(n))...")
+
+    if (n <= 1)
+    {
+        print("Terminal case, returning \(n).")
+        return 1
+    }
+    else
+    {
+        let nextFactorial = n - 1
+        let builtCalculation = n * calculateFactorial(from: nextFactorial)
+        print("Non-terminal case, returning \(builtCalculation).")
+        return builtCalculation
+    }
+}
+func main()
+{
+    print("The final result is: \(calculateFactorial(from: n)).")
+}
+main()
